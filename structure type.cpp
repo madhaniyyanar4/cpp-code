97a2c0cd8551980098d4394af0ec0470eb5ffaa4
madhaniyyanar4 <madhaniyyanar4@gmail.com>
2026-01-24 18:55:39 +0530

    Create structure type.cpp

---
+++ structure type.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+void main()
+{
+  int x=1;
+for(int i=0;i<=4;i++);
+  {
+    for (int j=0;j<=x;j++);
+      {
+        std::cout<<"1";
+      }
+      x=x+1
+        :endl;
+  }
