fd97d8092677a839d49182f354559a9fc0ae76a8
Madhubalatn9 <madhubalatn9@gmail.com>
2026-03-14 09:22:40 +0530

    commited

---
+++ operaters.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10,b=20;
+   
+    cout<<"the sum of a and b is "<<a+b<<endl;
+    cout<<"the difference of a and b is "<<a-b<<endl;
+    cout<<"the product of a and b is "<<a*b<<endl;    
+    cout<<"the quotient of a and b is "<<a/b<<endl;
+    return 0;
+}
