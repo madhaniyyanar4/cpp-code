6866b04b7ecbd9f59d7c3f742dac4edbd141147c
Madhubalatn9 <madhubalatn9@gmail.com>
2026-03-17 21:12:13 +0530

    commited

---
+++ if-else.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter a number:";
+    cin>>a;
+
+    if(a%2==0)
+    {
+        cout<<"the number is even";
+    }
+    else
+    {
+        cout<<"the number is odd";
+    }
+    return 0;
+}
