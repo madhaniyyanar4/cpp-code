15d80c542b8e3a85c03f832ab947b5da47731212
Madhubalatn9 <madhubalatn9@gmail.com>
2026-04-16 22:30:11 +0530

    added

---
+++ iterators.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> v={10,20,35,40};
+    vector<int>::iterator it;
+    for (it=v.begin();it!=v.end();it++)
+    {
+        cout<<*it<<endl;
+    }
+    return 0;
+}
